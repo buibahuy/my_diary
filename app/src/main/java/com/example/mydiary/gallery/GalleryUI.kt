a32723f67966560b4c6e5f4af12c2b04371ea1a4
Admin <buibahuy250399@gmail.com>
2023-08-28 18:38:29 +0700

    doing Gallery UI

--- app/src/main/java/com/example/mydiary/gallery/GalleryUI.kt
+++ app/src/main/java/com/example/mydiary/gallery/GalleryUI.kt
@@ -2,22 +2,51 @@ package com.example.mydiary.gallery
 
 import android.net.Uri
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.mydiary.datetime.formatLongToDate
 import com.example.mydiary.home.HomeViewModel
+import com.example.mydiary.ui.theme.Primary
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import androidx.compose.ui.layout.Layout
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -29,24 +58,117 @@ fun GalleryUI() {
         this.launch(Dispatchers.IO) { homeViewModel.getAllDiary() }
     }
 
-    val grouped = listDiary.groupBy {
-        it.time
+    val grouped = listDiary.sortedBy { it.time }.groupBy {
+        it.time.formatLongToDate("MM")
     }
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        grouped.forEach { (time, listDiary) ->
-            stickyHeader {
-                Text(text = time.formatLongToDate())
-            }
-            val listUri : MutableList<Uri> = mutableListOf()
-            listDiary.forEach {diary->
-                listUri.addAll( diary.photo.map {
-                    Uri.parse(it)
-                })
-            }
-            items(listUri){
-                AsyncImage(model = it, contentDescription = null)
-            }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+                .padding(vertical = 8.dp, horizontal = 12.dp),
+            text = "Gallery",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        LazyColumn(modifier = Modifier.weight(1f)) {
+            grouped.forEach { (time, listDiary) ->
+                stickyHeader {
+                    Text(text = time)
+                }
+                val listUri: MutableList<Uri> = mutableListOf()
+                listDiary.forEach { diary ->
+                    listUri.addAll(diary.photo.map {
+                        Uri.parse(it)
+                    })
+                }
+                item {
+//                    LazyVerticalGrid(
+//                        modifier = Modifier
+//                            .wrapContentHeight()
+//                            .padding(vertical = 12.dp),
+//                        columns = GridCells.Fixed(3),
+//                        verticalArrangement = Arrangement.spacedBy(8.dp),
+//                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+//                        content = {
+//                            items(listUri) {
+//                                AsyncImage(
+//                                    modifier = Modifier
+//                                        .clip(RoundedCornerShape(4.dp))
+//                                        .border(width = 2.dp, color = Color.White),
+//                                    model = it,
+//                                    contentDescription = null
+//                                )
+//                            }
+//                        }
+//                    )
+                    VerticalGrid(columns = 3) {
+                        listUri.forEach {
+                            AsyncImage(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(4.dp)
+                                    .clip(RoundedCornerShape(4.dp)),
+                                model = it,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                }
 
+            }
+        }
+    }
+
+}
+
+@Composable
+fun VerticalGrid(
+    modifier: Modifier = Modifier,
+    columns: Int = 2,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables, constraints ->
+        val itemWidth = constraints.maxWidth / columns
+        // Keep given height constraints, but set an exact width
+        val itemConstraints = constraints.copy(
+            minWidth = itemWidth,
+            maxWidth = itemWidth
+        )
+        // Measure each item with these constraints
+        val placeables = measurables.map { it.measure(itemConstraints) }
+        // Track each columns height so we can calculate the overall height
+        val columnHeights = Array(columns) { 0 }
+        placeables.forEachIndexed { index, placeable ->
+            val column = index % columns
+            columnHeights[column] += placeable.height
+        }
+        val height = (columnHeights.maxOrNull() ?: constraints.minHeight)
+            .coerceAtMost(constraints.maxHeight)
+        layout(
+            width = constraints.maxWidth,
+            height = height
+        ) {
+            // Track the Y co-ord per column we have placed up to
+            val columnY = Array(columns) { 0 }
+            placeables.forEachIndexed { index, placeable ->
+                val column = index % columns
+                placeable.placeRelative(
+                    x = column * itemWidth,
+                    y = columnY[column]
+                )
+                columnY[column] += placeable.height
+            }
         }
     }
 }
